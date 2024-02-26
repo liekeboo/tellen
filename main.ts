a2aa56fe62b680ade27ed9f1f88ba8fdb5ac58a4
liekeboo <160623259+liekeboo@users.noreply.github.com>
2024-02-26 11:29:57 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let getal = 0
+basic.forever(function () {
+    if (getal <= 15) {
+        basic.showNumber(getal)
+        getal += 1
+        basic.pause(500)
+    } else {
+        basic.clearScreen()
+        getal = 0
+    }
+})
+basic.forever(function () {
+    let tel = 0
+    if (tel % 2 == 1) {
+    	
+    } else if (false) {
+    	
+    }
+})
