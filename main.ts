ae687512e8b0890c2daada6326705b308d16b17b
liekeboo <160623259+liekeboo@users.noreply.github.com>
2024-02-27 16:03:25 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 let getal = 0
 basic.forever(function () {
+    let tel = 0
+    if (tel % 2 == 1) {
+        led.plot(4, 1)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
     if (getal <= 15) {
-        basic.showNumber(getal)
         getal += 1
         basic.pause(500)
     } else {
@@ -10,10 +17,32 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    let tel = 0
-    if (tel % 2 == 1) {
-    	
-    } else if (false) {
-    	
+    if (getal <= 15) {
+        basic.clearScreen()
+        basic.pause(1000)
+        led.plot(4, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+        led.plot(3, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+        led.plot(4, 0)
+        led.plot(3, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+        led.plot(2, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+        led.plot(2, 0)
+        led.plot(4, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+    } else {
+        basic.clearScreen()
     }
 })
